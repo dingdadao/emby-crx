671e26587a387589f2af0632e512a11a2091b8ec
dension <like_cosmos@163.com>
2025-07-15 17:43:00 +0800

    重新开始

--- script.sh
+++ script.sh
@@ -1,27 +1,70 @@
-#!/bin/bash
+#!/bin/sh
 
-# 创建emby-crx目录并下载所需文件
+set -e
+
+TS=$(date +%s)
+
+# === 下载 emby-crx 资源 ===
+
+echo "🧹 清理旧 emby-crx 目录..."
 rm -rf emby-crx
 mkdir -p emby-crx
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/css/style.css -P emby-crx/
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/common-utils.js -P emby-crx/
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/jquery-3.6.0.min.js -P emby-crx/
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/md5.min.js -P emby-crx/
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/content/main.js -P emby-crx/
 
-# 读取index.html文件内容
-content=$(cat index.html)
+echo "⬇️ 下载资源..."
+wget -q https://raw.githubusercontent.com/dingdadao/emby-crx/master/static/css/style.css -P emby-crx/
+wget -q https://raw.githubusercontent.com/dingdadao/emby-crx/master/static/js/common-utils.js -P emby-crx/
+wget -q https://raw.githubusercontent.com/dingdadao/emby-crx/master/static/js/jquery-3.6.0.min.js -P emby-crx/
+wget -q https://raw.githubusercontent.com/dingdadao/emby-crx/master/static/js/md5.min.js -P emby-crx/
+wget -q https://raw.githubusercontent.com/dingdadao/emby-crx/master/content/main.js -P emby-crx/
+# wget -q https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css -P emby-crx/
+# wget -q https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js -P emby-crx/
 
-# 检查index.html是否包含emby-crx
-if grep -q "emby-crx" index.html; then
-    echo "Index.html already contains emby-crx, skipping insertion."
-else
-    # 定义要插入的代码
-    code='<link rel="stylesheet" id="theme-css" href="emby-crx/style.css" type="text/css" media="all" />\n<script src="emby-crx/common-utils.js"></script>\n<script src="emby-crx/jquery-3.6.0.min.js"></script>\n<script src="emby-crx/md5.min.js"></script>\n<script src="emby-crx/main.js"></script>'
+echo "✅ 资源下载完成"
 
-    # 在</head>之前插入代码
-    new_content=$(echo -e "${content/<\/head>/$code<\/head>}")
+# === 插入 HTML 脚本块 ===
 
-    # 将新内容写入index.html文件
-    echo -e "$new_content" > index.html
+if [ ! -f index.html ]; then
+  echo "❌ 找不到 index.html 文件"
+  exit 1
 fi
+
+# 删除旧插入块
+sed -i '/<!-- EMBY-CRX-START -->/,/<!-- EMBY-CRX-END -->/d' index.html
+
+# 构造插入代码
+# cat > .emby_block.html <<EOF
+# <!-- EMBY-CRX-START -->
+# <link rel="stylesheet" href="emby-crx/swiper-bundle.min.css?v=$TS" />
+# <link rel="stylesheet" id="theme-css" href="emby-crx/style.css?v=$TS" type="text/css" media="all" />
+# <script src="emby-crx/common-utils.js?v=$TS"></script>
+# <script src="emby-crx/jquery-3.6.0.min.js?v=$TS"></script>
+# <script src="emby-crx/md5.min.js?v=$TS"></script>
+# <script src="emby-crx/main.js?v=$TS"></script>
+# <script src="emby-crx/swiper-bundle.min.js?v=$TS"></script>
+# <!-- EMBY-CRX-END -->
+# EOF
+cat > .emby_block.html <<EOF
+<!-- EMBY-CRX-START -->
+<link rel="stylesheet" id="theme-css" href="emby-crx/style.css?v=$TS" type="text/css" media="all" />
+<script src="emby-crx/common-utils.js?v=$TS"></script>
+<script src="emby-crx/jquery-3.6.0.min.js?v=$TS"></script>
+<script src="emby-crx/md5.min.js?v=$TS"></script>
+<script src="emby-crx/main.js?v=$TS"></script>
+<!-- EMBY-CRX-END -->
+EOF
+
+# 获取 </body> 行号
+BODY_LINE=$(grep -n '</body>' index.html | cut -d: -f1 | head -n 1)
+
+if [ -z "$BODY_LINE" ]; then
+  echo "❌ 未找到 </body> 标签，插入失败"
+  rm -f .emby_block.html
+  exit 1
+fi
+
+# 在 </body> 前插入
+sed -i "${BODY_LINE}r .emby_block.html" index.html
+
+rm -f .emby_block.html
+
+echo "✅ emby-crx 成功插入 index.html，时间戳：$TS"
